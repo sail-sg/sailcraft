b888846321a07e7552520bae004673f309ed0148
longxu <dreamerdeo247@gmail.com>
2024-04-29 01:21:49 +0800

    update count topk occurrences scripts & readme case

--- README.md
+++ README.md
@@ -5,23 +5,66 @@ It consists of data cleaning, near deduplication and exact deduplication.
 
 ### Requirements
 
+Install the package and download the models
 ```
 pip install -r requirements.txt
 mkdir lm_resource 
 python data_cleaning/download_sentencepiece_kenlm_models.py --output_dir_path lm_resource
 ```
 
+Install Rust for exact dedup, refer to [this guidance](https://github.com/google-research/deduplicate-text-datasets#installing).
+```
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
+```
+
 ### Quickstart
+
+We sample 1000 lines from [cc100](https://data.statmt.org/cc-100/) Indonesian subset for quick check.
 
 ```
 bash run_example.sh
 ```
 
+As expected, you should observe the following log
+```
+Counting lines in cleaned data output: 987
+Counting lines in near deduplication output: 974
+Counting lines in exact deduplication output: 979
+Counting lines in final output: 949
+```
+The final output are `/data/data_output/exact_dedup_output/sample/sample.jsonl`.
 
 ### Case Studies
 
 1. for data cleaning, check the `code/data_cleaning/filtering_logs` for each filter.
-2. run `exact_dedup/scripts/count_topk_occurrences.py` to obtain the top-k occurrences.
+
+2. run `code/exact_dedup/scripts/count_topk_occurrences.py` to obtain the top-k occurrences.
+```
+python code/exact_dedup/scripts/count_topk_occurrences.py \
+    --data_alias sample \
+    --split train \
+    --top_k_number 100 \
+    --threshold 2 \
+    --cache_dir cache/exact_dedup_cache 
+```
+This script displays the 100 most frequent spans with a frequency greater than two.
+
+
+| Count | Excerpt                                                                                           |
+|-------|---------------------------------------------------------------------------------------------------|
+| 4     | 'pernah disentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) dan' |
+| 4     | 'k pernah disentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) da' |
+| 4     | 'nah disentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) dan tid' |
+| 4     | 'sentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) dan tidak pul' |
+| 4     | 'uh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) dan tidak pula ol' |
+| 4     | 'ah disentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) dan tida' |
+| 4     | 'ak pernah disentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) d' |
+| 4     | 'ernah disentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) dan t' |
+| 3     | 'manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka) dan tidak pula oleh jin.'  |
+| 3     | 'tidak pernah disentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami mereka' |
+| 3     | 'tidak pernah disentuh oleh manusia sebelum mereka (penghuni-penghuni surga yang menjadi suami merek'  |
+
 
 
 ### Acknowledgement
--- code/exact_dedup/scripts/count_topk_occurrences.py
+++ code/exact_dedup/scripts/count_topk_occurrences.py
@@ -2,104 +2,102 @@
 import subprocess
 import shlex
 import os
+import math
 import glob
 from collections import OrderedDict, defaultdict
 from tqdm import tqdm
 import json
+import argparse
 
-a = 256**0
-b = 256**1
-c = 256**2
-d = 256**3
-e = 256**4
+# Powers of 256 precomputed for use in byte manipulations.
+powers_of_256 = [256**i for i in range(7)]  # Adjust the range as needed.
 
-def get_dups_pointers(dups_f, sizes_f, threshold=100):
+# Inspired by Yanai Elazar (@yanaiela).
+# Source: https://github.com/google-research/deduplicate-text-datasets/issues/20#issuecomment-1258518761
+# This function has been enhanced to adaptively calculate the size_of_pointer to prevent out-of-scope errors.
+def get_dups_pointers(dups_f, sizes_f, size_of_pointer, threshold=100):
+    # Read the dups file contents.
     with open(dups_f, 'rb') as f:
         dups = f.read()
 
+    # Read the sizes file contents.
     with open(sizes_f, 'rb') as f:
         sizes = f.read()
 
+    # Initialize the list for storing pointer data and frequency collections.
     pointers = []
     freq_collections = {}
 
+    # Create a map of integers based on the specified size of pointer.
+    int_map = powers_of_256[:size_of_pointer]
+
     # int manipulation
     extra = 0
-    for i in range(0, len(sizes), 5):
+    for i in range(0, len(sizes), size_of_pointer):
         count = 0
-        for j, mult in zip(range(5), [a,b,c,d,e]):
+        for j, mult in zip(range(size_of_pointer), int_map):
             count += sizes[i + j] * mult
-
         if count > threshold:
             pointer = 0
-            
-            for j, mult in zip(range(5), [a,b,c,d,e]):
+            for j, mult in zip(range(size_of_pointer), int_map):
                 pointer += dups[extra + j] * mult
 
             pointers.append(pointer)
             freq_collections[count] = pointer
 
-        extra += (5 * count)
+        extra += (size_of_pointer * count)
 
     sorted_dict =  dict(sorted(freq_collections.items(), reverse=True))
-    top_k_values = [value for _, value in list(sorted_dict.items())[:10]]
 
-    return pointers, sorted_dict, top_k_values
+    return pointers, sorted_dict
 
 if __name__ == "__main__":
-    data_alias = 'cleaned_cc100_ind_dedup_doc_1.train'
+    parser = argparse.ArgumentParser(description='Load a dataset.')
+    parser.add_argument('--data_alias', type=str, default='sample')
+    parser.add_argument('--split', type=str, default='train')
+    parser.add_argument('--top_k_number', type=int, default=100)
+    parser.add_argument('--threshold', type=int, default=100)
+    parser.add_argument('--cache_dir', type=str, default='../../cache/data_clean_cache')
+    args = parser.parse_args()
+
+    data_alias = "{}.{}".format(args.data_alias, args.split)
+    cache_dir=args.cache_dir
+    top_k_number = args.top_k_number
+    threshold = args.threshold
 
     print('====processing {}===='.format(data_alias))
-    
-    dups_prefix = 'tmp/cache/dups_{}'.format(data_alias)
-    sizes_prefix = 'tmp/cache/sizes_{}'.format(data_alias)
+
+    dups_prefix = os.path.join(cache_dir, 'dups_{}'.format(data_alias))
+    sizes_prefix = os.path.join(cache_dir,'sizes_{}'.format(data_alias))
+
     dups_files = sorted(glob.glob(f'{dups_prefix}*'))
     sizes_files = sorted(glob.glob(f'{sizes_prefix}*'))
+
+    ### Calculate the size of pointer based on the ratio of sizes between the raw data file and its suffix array.
+    ### The raw data file contains concatenated data sets.
+    ### The suffix array file aids in efficient text searches.
+    ### Documentation: https://github.com/google-research/deduplicate-text-datasets#finding-all-repeated-substrings-within-a-document
+    data_size = os.path.getsize(os.path.join(cache_dir, '{}'.format(data_alias)))
+    suffix_size = os.path.getsize(os.path.join(cache_dir, '{}.table.bin'.format(data_alias)))
+    size_of_pointer = math.ceil(suffix_size/data_size)
 
     print("There are {} files in total for {}".format(len(dups_files), data_alias))
 
     total_sorted_dict = defaultdict(int)
     for idx, (dups, sizes) in tqdm(enumerate(zip(dups_files, sizes_files))):
-        _, sorted_dict, _ = get_dups_pointers(dups, sizes)
+        _, sorted_dict = get_dups_pointers(dups, sizes, size_of_pointer, threshold)
         for key, value in sorted_dict.items():
             total_sorted_dict[value] += key
 
     # print(total_sorted_dict)
     total_sorted_dict = dict(sorted(total_sorted_dict.items(), key=lambda item: item[1], reverse=True))
-    top_k_number = 100
     top_k_cnt = list(total_sorted_dict.values())[:top_k_number]
     top_k_values = list(total_sorted_dict.keys())[:top_k_number]
     print(top_k_values, '\n', top_k_cnt)
 
-    with open(f'{data_alias}.json', 'w') as json_file:
-        json.dump(total_sorted_dict, json_file)
-    zipped_lists = zip(top_k_values, top_k_cnt)
-    with open(f'{data_alias}.txt', 'w') as f:
-        for value, cnt in zipped_lists:
-            f.write(f"{value}\t{cnt}\n")
-
-
-    # top_k_values = []
-    # top_k_cnt = []
-    # last_cnt = 0
-    # with open(f'{data_alias}.json', 'r') as f:
-    #     data = json.load(f)
-
-    #     for key, value in data.items():
-    #         if abs(value - last_cnt)<100:
-    #             continue
-    #         last_cnt = value
-
-    #         top_k_values.append(int(key))
-    #         top_k_cnt.append(int(value))
-
-    f=open("/sail-data/sealm/cleaned_data/dedup_exp/google_exp/{}".format(data_alias),"rb").read()
+    f=open(os.path.join(cache_dir, data_alias),"rb").read()
     for idx, (p, cnt) in enumerate(zip(top_k_values,top_k_cnt)):
-        if idx>10:
-            continue
-
         dedup_text = f[p:p+100]
-
         sdedup_text = dedup_text.decode('utf-8', errors='ignore')
         sdedup_text = sdedup_text.strip()
         sdedup_text = shlex.quote(sdedup_text)
